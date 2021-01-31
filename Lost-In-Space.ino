d8a562fef8da9c6b99f46bde314324f0e4f7652d
DominikG7 <78192992+DominikG7@users.noreply.github.com>
2021-01-30 20:39:20 -0800

    Update Lost-In-Space.ino

--- Lost-In-Space.ino
+++ Lost-In-Space.ino
@@ -113,15 +113,15 @@ void seedEnergy () {
 // Switch objects on long press
 void checkObjectSwap () {
   if (buttonLongPressed()) {
-    if (objectType == ASTEROID) {
+    if (objectType == PARTICLE) {
       objectType = SHIP;
-      fuel = STARTINGFUEL;
-      fuelColor = STARTINGFUELCOLOR;
-      fuelConsumption.set(FUELTIMERDELAYMS);
-      burnRate = FUELDECREMENT;
-      fuelIndexShift = random(6);
+      energy = STARTINGENERGY;
+      energyColor = STARTINGENERGYCOLOR;
+      energyDecay.set(ENERGYTIMERDELAYMS);
+      decayRate = ENERGYDECREMENT;
+      energyIndexShift = random(6);
     } else {
-      makeAsteroid();
+      makeParticle();
     }
   }
 }
@@ -130,7 +130,7 @@ void checkObjectSwap () {
 void checkScan () {
   if (shipConnected()) {
     if (!isScanned){
-      seedFuel();
+      seedEnergy();
     }
     scanFading = false;
     isScanned = true;
@@ -160,29 +160,29 @@ bool shipConnected () {
   return shipFound;
 }
 
-// Return true if asteroid is connected
-bool asteroidConnected () {
-  bool asteroidFound = false; 
+// Return true if particle is connected
+bool particleConnected () {
+  bool particleFound = false; 
   FOREACH_FACE(f) {
-    if (isAsteroidOnFace(f)) {
-      asteroidFound = true;
+    if (isParticleOnFace(f)) {
+      particleFound = true;
     }
   }
-  return asteroidFound;
+  return particleFound;
 }
 
-void checkFuelSend () {
+void energyFuelSend () {
   FOREACH_FACE(f) {
-    if (gatherTimer.isExpired() && sendingOnFace[f]) {
+    if (absorbTimer.isExpired() && sendingOnFace[f]) {
         sendingOnFace[f] = false;
-        fuelContents[f] = NONE;
+        energyContents[f] = NONE;
     }
   }
   if (shipConnected() && buttonPressed()) {
     FOREACH_FACE(f) {
-      if (fuelContents[f] != NONE && isShipOnFace(f)) {
+      if (energyContents[f] != NONE && isShipOnFace(f)) {
         sendingOnFace[f] = true;
-        gatherTimer.set(GATHERTIMEMS);
+        absorbTimer.set(ABSORBTIMEMS);
       }
     }
   }
@@ -193,23 +193,23 @@ void checkFuelSend () {
   }
 }
 
-void checkFuelReceive() {
+void checkEnergyReceive() {
   FOREACH_FACE(f) {
-    if (isAsteroidOnFace(f) && sendingFuelOnFace(f) && !receivedOnFace[f]) {
-      int receivedFuelType = getFuelContents(getLastValueReceivedOnFace(f));
-      // Set fuel color to new fuel color
-      fuelColor = fuelColors[receivedFuelType];
+    if (isParticleOnFace(f) && sendingEnergyOnFace(f) && !receivedOnFace[f]) {
+      int receivedEnergyType = getEnergyContents(getLastValueReceivedOnFace(f));
+      // Set energy color to new energy color
+      energyColor = energyColors[receivedEnergyType];
       // Determine actual shifted index
-      receivedFuelType = ((receivedFuelType - 1) + fuelIndexShift) % 6;
-      // Add fuel load
-      fuel = fuel + fuelLoads[receivedFuelType];
-      // Set burn rate
-      burnRate = fuelBurns[receivedFuelType];
+      receivedEnergyType = ((receivedEnergyType - 1) + energyIndexShift) % 6;
+      // Add energy load
+      energy = energy + energyLoads[receivedEnergyType];
+      // Set decay rate
+      decayRate = energyDecays[receivedEnergyType];
       // Mark received on face
       receivedOnFace[f] = true;
     }
   }
-  if (!asteroidConnected()) {
+  if (!particleConnected()) {
     FOREACH_FACE(f) {
       receivedOnFace[f] = false;
     }
@@ -229,8 +229,8 @@ bool isShipOnFace (int f) {
   }
 }
 
-// Check face for asteroid
-bool isAsteroidOnFace (int f) {
+// Check face for particle
+bool isParticleOnFace (int f) {
   if (!isValueReceivedOnFaceExpired(f)){
     if (getObjectType(getLastValueReceivedOnFace(f)) == 0) {
       return true;
@@ -242,9 +242,9 @@ bool isAsteroidOnFace (int f) {
   }
 }
 
-bool sendingFuelOnFace (int f) {
+bool sendingEnergyOnFace (int f) {
   if (!isValueReceivedOnFaceExpired(f)){
-    if (getFuelContents(getLastValueReceivedOnFace(f)) != NONE) {
+    if (getEnergyContents(getLastValueReceivedOnFace(f)) != NONE) {
       return true;
     } else {
       return false;
@@ -254,12 +254,12 @@ bool sendingFuelOnFace (int f) {
   }
 }
 
-// See if fuel should be decreased or is now empty
-void checkFuelConsumption () {
-  if ( fuel > 0 ) {
-    if (fuelConsumption.isExpired()) {
-      fuel = fuel - burnRate;
-      fuelConsumption.set(FUELTIMERDELAYMS);
+// See if energy should be decreased or is now empty
+void checkEnergyDecay () {
+  if ( energy > 0 ) {
+    if (energyDecay.isExpired()) {
+      energy = energy - decayRate;
+      energyConsumption.set(ENERGYTIMERDELAYMS);
     }
   } else {
     fuel = 0;
@@ -278,7 +278,7 @@ void commsHandler () {
   } else {
     FOREACH_FACE(f) {
       if (sendingOnFace[f]) {
-        setValueSentOnFace((fuelContents[f] << 3) + (gameMode << 1) + 0, f);
+        setValueSentOnFace((energyContents[f] << 3) + (gameMode << 1) + 0, f);
       } else {
         setValueSentOnFace((NONE << 3) + (gameMode << 1) + 0, f);
       }
@@ -294,39 +294,39 @@ byte getGameMode (byte data) {
   return ((data >> 1) & 3);
 }
 
-byte getFuelContents (byte data) {
+byte getEnergyContents (byte data) {
   return ((data >> 3) & 7);
 }
 
 // Handle LED display
 void displayHandler () {
   if (objectType == SHIP) { // Ship display handler
-    // Sliding fuel display calculations
-    int fuelPerLED = round(FUELMAX / 6); // Amount of fuel per LED
-    int fullLEDs = round(fuel / fuelPerLED); // How many LEDs are 100% full
-    int remainder = round(fuel % fuelPerLED); // remaining fuel in partially full LED
+    // Sliding energy display calculations
+    int energyPerLED = round(ENERGYMAX / 6); // Amount of energy per LED
+    int fullLEDs = round(energy / energyPerLED); // How many LEDs are 100% full
+    int remainder = round(energy % energyPerLED); // remaining energy in partially full LED
     FOREACH_FACE(f) {
       if (f < (fullLEDs)) {
-        setColorOnFace(fuelColor, f); // Make full LEDs lit
+        setColorOnFace(energyColor, f); // Make full LEDs lit
       } else if (f > (fullLEDs)){
         setColorOnFace(OFF, f); // Make empty LEDs off
       } else {
-        setColorOnFace(dim(fuelColor,map(remainder,0,fuelPerLED,0,255)), f); // set partial brightness to partially full LED, mapped to 255 scale
+        setColorOnFace(dim(energyColor,map(remainder,0,energyPerLED,0,255)), f); // set partial brightness to partially full LED, mapped to 255 scale
       }
     }
-  } else { // Asteroid display handler
-    if (!isScanned) { // Unscanned asteroids display as blank (for now)
+  } else { // Particle display handler
+    if (!isScanned) { // Unscanned particles display as blank (for now)
       FOREACH_FACE (f) {
         setColorOnFace(UNSCANNED, f);
       }
     } else if (!scanFading) { // Scanned but not fading (usually because ship still connected) displays as full brightness
       FOREACH_FACE (f) {
-        setColorOnFace(fuelColors[fuelContents[f]],f);
+        setColorOnFace(energyColors[energyContents[f]],f);
       }
     } else { // Scanned and fading, dim to match fading scan
       int scanRemaining = map(scanDecay.getRemaining(),0,SCANDECAYMS,0,255);
       FOREACH_FACE (f) {
-      setColorOnFace(dim(fuelColors[fuelContents[f]],scanRemaining),f);
+      setColorOnFace(dim(energyColors[energyContents[f]],scanRemaining),f);
       }
     }
   }
